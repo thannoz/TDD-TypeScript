c612c54bbc7cd8ebeba23bb72b3ea8032dc2a766
thannoz <carlos-7@hotmail.de>
2022-09-08 01:24:02 +0200

    Add two unit tests in mathFunctions.test.ts

--- src/mathFunctions.test.ts
+++ src/mathFunctions.test.ts
@@ -1,8 +1,11 @@
 import { describe, expect, test } from "@jest/globals";
-import { add } from "./mathFunctions";
+import { add, multiply } from "./mathFunctions";
 
 describe("Math functions tests", () => {
   it("Check add function", () => {
     expect(add(1, 2)).toBe(3);
+  });
+  it("Check multiply function", () => {
+    expect(multiply(4, 4)).toBe(16);
   });
 });
